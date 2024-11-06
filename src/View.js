0c31226601754c619af8f45c9006f84cd5e07526
eunjin6-6 <dmswls616@gmail.com>
2024-11-06 12:00:33 +0900

    view.js 함수형 변경 완료

--- src/View.js
+++ src/View.js
@@ -1,62 +1,49 @@
-import React, { Component } from 'react'
-import Form from 'react-bootstrap/Form';
+import React, { useEffect, useState } from 'react'
 import Button from 'react-bootstrap/Button';
 import Axios from "axios";
-import { Link } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 
+const View = ()=>{
+  //초기값 설정, 변수에 담기
+  const [board, setBoard] = useState(null);
+  const navigate = useNavigate();
+  let { id } = useParams();
+  //console.log(id); 에러찾기
 
-export default class View extends Component {
-  state= {
-    title:'',
-    content:''
-  }
+  
+    useEffect(()=>{
+      console.log('useEffect 실행');
+      Axios.get(`http://localhost:8000/detail?id=${id}`)
+      .then((res) => {
+        const {data} = res;  //destructuring 비구조할당으로 변경
+        
+        setBoard({
+          title: data[0].BOARD_TITLE,
+          content: data[0].BOARD_CONTENT
+        });
 
-
- 
-  detail = () =>{
-    let url = window.location.href; //현재주소
-    //console.log(url); // http://localhost:3000/view?id=1
-    let urlParams = url.split('?')[1];
-    //console.log(urlParams); //id=1
-    const searchParams = new URLSearchParams(urlParams); //{id:1} 객체로
-    console.log(urlParams);
-    let id = searchParams.get('id'); //1
-
-    Axios.get(`http://localhost:8000/detail?id=${id}`)
-    .then((res) => {
-      const {data} = res;  //destructuring 비구조할당으로 변경
-      console.log(data);
-      this.setState({
-        title: data[0].BOARD_TITLE,
-        content: data[0].BOARD_CONTENT
       })
-    })
-    .catch((e)=> {
-      // 에러 핸들링
-      console.log(e);
-    });     
-  }
-  //this.prop.isModifyMode에 변동사항이 생기면 detail 함수 실행, componentDidUpdate 함수로 
+      .catch((e)=> {
+        // 에러 핸들링
+        console.log(e);
+      });     
+    },[id]); //id값이 바뀌면
+    
+    console.log(board);
+
+    //로딩되는 시간 벌어줌
+    if(!board) return <div>Loading...</div>; 
 
 
-  componentDidMount() {
-      this.detail();
-  }
-
-
-  render() {
- 
-    return (      
-      <div>
-        <h2>{this.state.title}</h2>
-        <h3>{this.state.content}</h3>
-        <hr/>
-        <Link to="/" className="btn btn-secondary">
-          목록
-        </Link>
-      </div>
-
-
-    )
-  }
+  return(
+    <div>
+      <h2>{board.title}</h2>
+      <h3>본문: {board.content}</h3>
+      <hr/>
+      <Button variant="btn btn-secondary" onClick={()=>{navigate(-1)}}>
+        목록
+      </Button>
+    </div>
+  )
 }
+export default View;
