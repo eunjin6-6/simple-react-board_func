2a70aa011c75b9c23679fc5c5faaf2057c00ec74
eunjin6-6 <dmswls616@gmail.com>
2024-11-06 11:08:14 +0900

    훅에서 Component 삭제, 주석 정리

--- src/BoardList.js
+++ src/BoardList.js
@@ -1,4 +1,4 @@
-import React, { Component, useCallback, useEffect, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Axios from 'axios';
@@ -97,8 +97,6 @@ const BoardList = ({isComplete, handleModify})=>{
   }
 
 
-
-
   return(
     <>
       <Table striped bordered hover>
@@ -140,107 +138,3 @@ const BoardList = ({isComplete, handleModify})=>{
 }
 
 export default BoardList;
-
-
-/*
-
-export default class BoardList extends Component {
-
-  onCheckboxChange = (checked, id)=>{
-    const list = [...this.state.checkList]; //복사본을 만들어서 값 넣고, 그걸 교체하는게 바람직함
-    if(checked){
-      if(!list.includes(id)){
-        list.push(id);
-      }
-    }else{
-      let idx = list.indexOf(id);
-      list.splice(idx, 1)
-    }
-
-    this.setState({
-      checkList:list
-    });
-    console.log(this.state.checkList);
-  }
-
-
-  getList = ()=>{
-    Axios.get('http://localhost:8000/list')
-    .then((res)=>{
-      // 성공 핸들링
-      //const data = res.data; 요청해서 받아온값
-      const {data} = res; //destructuring 비구조 할당으로 변경
-      //console.log(data);
-      this.setState({
-        BoardList:data
-      });
-      this.props.renderComplete(); //App.js에 목록 출력이 완료되었다고 전달
-    
-    })
-    .catch((e)=>{
-      // 에러 핸들링
-      console.log(e);
-    });
-  }
-  componentDidMount(){
-    this.getList();
-  }
-
-
-  componentDidUpdate(prevProps) {
-    // 목록 다시 조회
-    if (this.props.isComplete !== prevProps.isComplete) {
-      this.getList();
-    }
-  }
-
-  
-
-
-
-  render() {
-    //console.log(this.props)
-    //console.log(this.state.BoardList[0].BOARD_TITLE);
-    return (
-      <>
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>선택</th>
-              <th>번호</th>
-              <th>제목</th>
-              <th>작성자</th>
-              <th>작성일</th>
-            </tr>
-          </thead>
-          <tbody>
-            {
-              this.state.BoardList.map(
-                item=><Board 
-                  key={item.BOARD_ID} 
-                  id={item.BOARD_ID} 
-                  title={item.BOARD_TITLE} 
-                  registerId={item.REGISTER_ID} 
-                  date={item.REGISTER_DATE}
-                  onCheckboxChange={this.onCheckboxChange}
-                />
-              )
-            }
-          </tbody>
-        </Table>
-        <div className="d-flex gap-1">
-          <Link to="/write" className="btn btn-primary">
-           글쓰기
-          </Link>
-          <Button variant="secondary" onClick={()=>{
-            this.props.handleModify(this.state.checkList);
-          }}>수정하기</Button>
-          <Button variant="danger" onClick={()=>{this.handleDelete();}}>삭제하기</Button>
-        </div>
-      </>
-    
-    )
-  }
-}
-
-/* */
